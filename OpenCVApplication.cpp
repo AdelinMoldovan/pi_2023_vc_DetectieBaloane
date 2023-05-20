141e5709155c1e0198178db604e4c3316b831280
Mihai-Rares <mihai.rares.dn@gmail.com>
2023-05-21 02:50:22 +0300

    Implemented image segmentation and filtered resulting objects based on area and circularity criteria.

--- OpenCVApplication.cpp
+++ OpenCVApplication.cpp
@@ -11,6 +11,7 @@
 #include <set>
 #include <random>
 #include <functional>
+#include <type_traits>
 
 void testOpenImage()
 {
@@ -105,6 +106,55 @@ void detectLines() {
 	}
 }
 
+inline Vec3b bgr_to_hsv(Vec3b pixel) {
+	float r = pixel[2];
+	float g = pixel[1];
+	float b = pixel[0];
+	r /= 255;
+	g /= 255;
+	b /= 255;
+	float M = max(max(r, g), b);
+	float m = min(min(r, g), b);
+	float C = M - m;
+	float V = M;
+	float S;
+	float H;
+	if (V != 0)
+		S = C / V;
+	else
+		S = 0;
+
+	if (C != 0) {
+		if (M == r) H = 60 * (g - b) / C;
+		if (M == g) H = 120 + 60 * (b - r) / C;
+		if (M == b) H = 240 + 60 * (r - g) / C;
+	}
+	else
+		H = 0;
+	if (H < 0)
+		H = H + 360;
+	Vec3b result;
+	result[0] = H * 255 / 360;
+	result[1] = S * 255;
+	result[2] = V * 255;
+	return result;
+}
+cv::Vec3b hsv_to_bgr(cv::Vec3b hsv_pixel) {
+	cv::Mat_<cv::Vec3b> hsv_mat(1, 1, hsv_pixel);
+	cv::Mat_<cv::Vec3b> bgr_mat;
+	cv::cvtColor(hsv_mat, bgr_mat, cv::COLOR_HSV2BGR);
+	return bgr_mat(0, 0);
+}
+cv::Vec3b lab_to_bgr(cv::Vec3b lab_pixel)
+{
+	cv::Mat lab_mat(1, 1, CV_8UC3);
+	lab_mat.at<cv::Vec3b>(0, 0) = lab_pixel;
+	cv::Mat bgr_mat;
+	cv::cvtColor(lab_mat, bgr_mat, cv::COLOR_Lab2BGR);
+	auto pixel = bgr_mat.at<cv::Vec3b>(0, 0);
+	return { (uchar)pixel[0],  (uchar)pixel[1],  (uchar)pixel[2] };
+}
+
 std::tuple<cv::Mat, cv::Mat, cv::Mat> convertToHSV(const cv::Mat& inputImage) {
 	int height = inputImage.rows;
 	int width = inputImage.cols;
@@ -154,13 +204,17 @@ std::tuple<cv::Mat, cv::Mat, cv::Mat> convertToHSV(const cv::Mat& inputImage) {
 inline boolean inside(Mat img, int r, int c) {
 	return (r < img.rows) && (r >= 0) && (c >= 0) && (c < img.cols);
 }
+template <typename ColorType>
 class Blob {
 public:
 	int area;
 	float perimeter;
 	float circularity;
+	ColorType color;
 	Blob(int area, float perimeter, float circularity) : 
 		area(area), perimeter(perimeter), circularity(circularity) {};
+	Blob(int area, float perimeter, float circularity, ColorType color) :
+		area(area), perimeter(perimeter), circularity(circularity), color(color) {};
 };
 inline boolean similar(Vec3b a, Vec3b b, float similarity_th) {
 	return fabs(a[0] - b[0]) <= similarity_th &&
@@ -211,17 +265,32 @@ END:
 inline double circularity(int area, double perimeter) {
 	return PI * 4 * area / (perimeter * perimeter);
 }
-std::tuple <Mat_<int>, std::map<int, Blob>> regionGrowing(Mat_<Vec3b> img, const float tolerance) {
+
+template<typename T>
+class BlobReader {
+public:
+	virtual void readPixel(const T& pixel) = 0;
+	virtual bool readPixel(const T& pixel, const T& parentPixel) = 0;
+	virtual Blob<T> getBlob() = 0;
+	virtual ~BlobReader() {};
+};
+template<typename PixelType, typename BlobReaderType, typename BoolConvertible>
+std::tuple <Mat_<int>, std::map<int, Blob<PixelType>>> 
+regionGrowing(const Mat_<PixelType>& img, const Mat_<BoolConvertible>& seedPoints) {
+
+	static_assert(std::is_base_of<BlobReader<PixelType>, BlobReaderType>::value, "Required BlobReader");
+
 	static Point neighbours[] = { { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 }, { -1, -1 }, { -1, 1 }, { 1, -1 }, { 1, 1 } };
-	std::map<int, Blob> blobs;
-	float similarity_th = tolerance / 100.0 * 255.0;
+	std::map<int, Blob<PixelType>> blobs;
+	long background_area = img.rows * img.cols;
 	int label = 0;
 	Mat_<int> labels = Mat_<int>::zeros(img.rows, img.cols);
 	for (int i = 0; i < img.rows; ++i) {
 		for (int j = 0; j < img.cols; ++j) {
-			if (labels(i, j) == 0) {
+			if (labels(i, j) == 0 && seedPoints(i, j)) {
+				BlobReaderType reader;
+				reader.readPixel(img(i, j));
 				++label;
-				int area = 1;
 				std::queue<Point> q;
 				labels(i, j) = label;
 				q.push({ j, i });
@@ -232,17 +301,21 @@ std::tuple <Mat_<int>, std::map<int, Blob>> regionGrowing(Mat_<Vec3b> img, const
 						Point neighbour = p + neighbours[k];
 						if (inside(img, neighbour.y, neighbour.x) &&
 							labels(neighbour) == 0 &&
-							similar(img(neighbour), img(p), similarity_th)) {
+							//similar(img(neighbour), img(p), similarity_th)
+							reader.readPixel(img(neighbour), img(p))
+							) {
 							labels(neighbour.y, neighbour.x) = label;
 							q.push(neighbour);
-							++area;
 						}
 					}
 				}
-				blobs.insert({ label, Blob(area, 0,  0) });
+				auto blob = reader.getBlob();
+				background_area -= blob.area;
+				blobs.insert({ label, blob});
 			}
 		}
 	}
+	blobs.insert({ 0, {background_area,0,0, {255, 255, 255}} });
 	return std::make_tuple(labels, blobs);
 }
 
@@ -252,11 +325,13 @@ Vec3b randomColor() {
 	static std::uniform_int_distribution<> distribution(0, 255);
 	return Vec3b(distribution(generator), distribution(generator), distribution(generator));
 }
-void computeBlobs(std::map<int, Blob> &blobs, Mat_<int> &labels, int background = -1) {
+
+template<typename T>
+void computeBlobs(std::map<int, Blob<T>> &blobs, Mat_<int> &labels, int background = -1) {
 	static Point neighbours[] = { { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 }, { -1, -1 }, { -1, 1 }, { 1, -1 }, { 1, 1 } };
 	std::set<int> processed;
-	//processed.insert(background);
 	Mat_<uchar> visited = Mat_<uchar>::zeros(labels.rows, labels.cols);
+	long background_area = labels.rows * labels.cols;
 	for (int i = 0; i < labels.rows; ++i) {
 		for (int j = 0; j < labels.cols; ++j) {
 			int label = labels(i, j);
@@ -279,6 +354,7 @@ void computeBlobs(std::map<int, Blob> &blobs, Mat_<int> &labels, int background 
 						}
 					}
 				}
+				background_area -= area;
 				float p = perimeter(labels, label);
 				auto& blob = blobs.find(label)->second;
 				blob.area = area;
@@ -287,6 +363,7 @@ void computeBlobs(std::map<int, Blob> &blobs, Mat_<int> &labels, int background 
 			}
 		}
 	}
+	blobs.insert({ 0, {background_area, 0, 0, {255, 255, 255}} });
 }
 bool operator<(const cv::Vec3b& lhs, const cv::Vec3b& rhs) {
 	return lhs[0] < rhs[0] ||
@@ -370,11 +447,11 @@ Mat_<T> separate(const Mat_<T> img, const Point& seed, const T background) {
 }
 
 template <typename T, typename Compare = std::less<T>>
-Mat_<T> map(
+Mat_<T> map_objects(
 	const Mat_<T> &img, 
 	T background, 
 	std::function<Mat_<T>(Mat_<T>)> map_object, 
-	std::function<void(Mat_<T>&, const Mat_<T>, const T)> mix) {
+	std::function<void(Mat_<T>&, const Mat_<T>&, const T)> mix) {
 	
 	std::set<T, Compare> processed;
 	Mat_<T> dst(img.rows, img.cols, background);
@@ -393,38 +470,170 @@ Mat_<T> map(
 	}
 	return dst;
 }
+template<typename T, typename V>
+Mat_<V> map_pixels(const Mat_<T> img, std::function<V(T)> f) {
+	Mat_<V> result(img.rows, img.cols);
+	for (int i = 0; i < img.rows; ++i) {
+		for (int j = 0; j < img.cols; ++j) {
+			result(i, j) = f(img(i, j));
+		}
+	}
+	return result;
+}
+class MonochromeLabBlobReader : public BlobReader<Vec3b> {
+private:
+	const long AA_TH = 17, BA_TH = 17, A_TH = 2, B_TH = 2;
+	long l, a, b;
+	long area;
+public:
+	void readPixel(const Vec3b& pixel) override {
+		l = pixel[0];
+		a = pixel[1];
+		b = pixel[2];
+		area = 1;
+	}
+	bool readPixel(const Vec3b& pixel, const Vec3b& parentPixel) override {
+		double newl = pixel[0];
+		double newa = pixel[1];
+		double newb = pixel[2];
+		double avga = a / area;
+		double avgb = b / area;
+		if (std::abs(avga - newa) > AA_TH ||
+			std::abs(avgb - newb) > BA_TH ||
+			std::abs(parentPixel[1] - pixel[1]) > A_TH ||
+			std::abs(parentPixel[2] - pixel[2]) > B_TH) {
+			return false;
+		}
+		l += newl;
+		a += newa;
+		b += newb;
+		area++;
+		return true;
+	}
+	Blob<Vec3b> getBlob() override {
+		auto color = lab_to_bgr({ static_cast<uchar>(l / area),
+													static_cast<uchar>(a / area),
+													static_cast<uchar>(b / area) });
+		return Blob<Vec3b>(area, 0, 0, color);
+	}
+};
+class MonochromeHSVBlobReader : public BlobReader<Vec3b> {
+private:
+	const long HA_TH = 60, SA_TH = 60, H_TH = 4, S_TH = 4;
+	long h, s, v;
+	long area;
+public:
+	void readPixel(const Vec3b& pixel) override {
+		h = pixel[0];
+		s = pixel[1];
+		v = pixel[2];
+		area = 1;
+	}
+	bool readPixel(const Vec3b& pixel, const Vec3b& parentPixel) override {
+		long newh = pixel[0];
+		long news = pixel[1];
+		long newv = pixel[2];
+		long avgh = h / area;
+		long avgs = s / area;
+		if (std::abs(avgh - newh) > HA_TH || 
+			std::abs(avgs - news) > SA_TH || 
+			std::abs(parentPixel[0] - pixel[0]) > H_TH || 
+			std::abs(parentPixel[1] - pixel[1]) > S_TH) {
+			return false;
+		}
+		h += newh;
+		s += news;
+		v += newv;
+		area++;
+		return true;
+	}
+	Blob<Vec3b> getBlob() override {
+		return Blob<Vec3b>(area, 0, 0, hsv_to_bgr({static_cast<uchar>(h / area), 
+										static_cast<uchar>(s / area), 
+										static_cast<uchar>(v / area)}));
+	}
+};
+class MonochromeRGBBlobReader : public BlobReader<Vec3b> {
+private:
+	const long	RA_TH = 60, GA_TH = 60, BA_TH = 60, 
+				R_TH  = 5,  G_TH  = 5,  B_TH  = 5;
+	long r, g, b;
+	long area;
+public:
+	void readPixel(const Vec3b& pixel) override {
+		b = pixel[0];
+		g = pixel[1];
+		r = pixel[2];
+		area = 1;
+	}
+	bool readPixel(const Vec3b& pixel, const Vec3b& parentPixel) override {
+		long newr = pixel[2];
+		long newg = pixel[1];
+		long newb = pixel[0];
+		long avgr = r / area;
+		long avgg = g / area;
+		long avgb = b / area;
+		if (std::abs(avgr - newr) > RA_TH ||
+			std::abs(avgg - newg) > GA_TH ||
+			std::abs(avgb - newb) > BA_TH ||
+			std::abs(parentPixel[0] - pixel[0]) > B_TH ||
+			std::abs(parentPixel[1] - pixel[1]) > G_TH ||
+			std::abs(parentPixel[2] - pixel[2]) > R_TH) {
+			return false;
+		}
+		r += newr;
+		g += newg;
+		b += newb;
+		area++;
+		return true;
+	}
+	Blob<Vec3b> getBlob() override {
+		return Blob<Vec3b>(area, 0, 0, { static_cast<uchar>(b / area),
+										static_cast<uchar>(g / area),
+										static_cast<uchar>(r / area) });
+	}
+};
 void detectBalloons() {
 	Mat image;
 	char fname[MAX_PATH];
 	while (openFileDlg(fname)) {
 		Mat clr = imread(fname, IMREAD_COLOR);
-		Mat img = imread(fname, IMREAD_GRAYSCALE);
+		Mat lab;
+		cv::cvtColor(clr, lab, cv::COLOR_BGR2Lab);
 		imshow("Initial image", clr);
-		auto segmented_data = regionGrowing(clr, 2);
+		auto seedPoints = map_pixels<Vec3b, uchar>(lab, [](Vec3b pixel) {
+			return (pixel[0] > 30 && pixel[0] < 210)?255:0;
+			});
+		auto segmented_data = regionGrowing<Vec3b, MonochromeLabBlobReader, uchar>(lab, seedPoints);
 		Mat_<int> segmented_image = std::get<0>(segmented_data);
 		auto& blobs = std::get<1>(segmented_data);
 		Mat_<int> objects = filter<int>(segmented_image, 0,
 			[&blobs](int label) {
 				int area = blobs.find(label)->second.area;
-				if (area >= 0) return true;
+				if (area >= 1000) return true;
 				return false;
 			});
-		Mat imgsg = colorCodeLabels(objects);
-		imshow("Segmentation", imgsg);
 		
 		Mat mat;
 		objects.convertTo(mat, CV_32F);
-		Mat kernel = getStructuringElement(MORPH_ELLIPSE, Size(15, 15));
+		Mat kernel = getStructuringElement(MORPH_ELLIPSE, Size(10, 10));
 		Mat closed;
 		morphologyEx(mat, closed, MORPH_CLOSE, kernel);
 		closed.convertTo(objects, CV_32S);
-		blobs.insert({ 0, {0,0,0} });
+		Mat imgsg = map_pixels<int, Vec3b>(objects, [&blobs](int label) {
+			return blobs.find(label)->second.color;
+			});
+		imshow("Segmentation", imgsg);
+		/*waitKey(0);
+		continue;*/
 		computeBlobs(blobs, objects, 0);
 		auto roundObjects = filter<int>(objects, 0,
 			[&blobs](int label) {
 				return blobs.find(label)->second.circularity > 0.65;
 			});
-		Mat finalImage = colorCodeLabels(roundObjects);
+		Mat finalImage = map_pixels<int, Vec3b>(roundObjects, [&blobs](int label) {
+			return blobs.find(label)->second.color;
+			}); 
 		imshow("Balloons", finalImage);
 		waitKey(0);
 	}
